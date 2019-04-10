5dd2761bdfe177cba4f873171d6dcd9c7a69f5e8
mahitha <mahitha.reddy132@gmail.com>
2019-04-10 13:09:26 +0530

    adding construction cost

---
+++ construction.java
@@ -0,0 +1,20 @@
+package task23;
+
+public class construction {
+
+public double findcost(int materialType,double areaOfHouse,boolean isAutomated)
+{
+	int cost=0;
+	if(materialType==0)
+		cost=1200;
+	else if(materialType==1)
+	cost=1500;
+	else if(materialType==2 && isAutomated)
+		cost=2500;
+	else
+		cost=1000;
+	return areaOfHouse*cost;
+
+	
+}
+}
